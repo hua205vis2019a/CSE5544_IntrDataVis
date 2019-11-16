030338d5a78d49337548e5f40dffcaed03738c91
hua205vis2019a <42682444+hua205vis2019a@users.noreply.github.com>
2019-11-15 22:37:14 -0500

    Add files via upload

--- Assign6/finalProject.py
+++ Assign6/finalProject.py
@@ -3,4 +3,6 @@ import pandas as pd
 emission_raw = pd.read_csv("emission data.csv")
 population_raw = pd.read_csv("country_population.csv")
 temperature_raw = pd.read_csv("GlobalLandTemperaturesByCountry.csv")
+lat_long = pd.read_csv("worldcitiespop.csv")
 
+print(lat_long)
